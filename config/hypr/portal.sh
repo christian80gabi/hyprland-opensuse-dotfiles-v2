461cff924902da130f7e274e08cc2e6a1aa7a9f1
Christian Gabi <49684157+christian80gabi@users.noreply.github.com>
2024-08-02 13:17:15 +0100

    Update portal.sh

--- config/hypr/portal.sh
+++ config/hypr/portal.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
+
 sleep 1
+echo "Kill portals..."
 killall -e xdg-desktop-portal-hyprland
 killall -e xdg-desktop-portal-wlr
 killall xdg-desktop-portal
-/usr/lib/xdg-desktop-portal-hyprland &
+
+sleep 1
+echo "Start portals..."
+/usr/libexec/xdg-desktop-portal-hyprland &
 sleep 2
-/usr/lib/xdg-desktop-portal &
+echo "XDG Portal..."
+/usr/libexec/xdg-desktop-portal &
+sleep 2
+echo "WLR Portal..."
+/usr/libexec/xdg-desktop-portal-wlr &
+sleep 2
+echo "KDE portal..."
+/usr/libexec/xdg-desktop-portal-kde &
