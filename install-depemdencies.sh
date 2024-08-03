d2e51dd04ab43c6677839e5371d52a185a82a71a
christian80gabi <christian-gabriel.nana@outlook.com>
2024-08-03 15:13:49 +0100

    dependencies: add some missed dependencies such as hyprcursor

--- install-depemdencies.sh
+++ install-depemdencies.sh
@@ -1,7 +1,7 @@
 !/usr/bin/env bash
 
-echo "Installing openSUSEway and dependecies. (This will configure a lot for you)"
-sudo zypper install -y --recommends openSUSEway patterns-openSUSEway qt6ct btop blueman
+echo "Installing openSUSEway and dependencies. (This will configure a lot for you)"
+sudo zypper install -y --recommends openSUSEway patterns-openSUSEway qt6ct btop blueman clipman 
 
 echo "Installing Hyprland"
-sudo zypper install -y hyprland hyprpaper
+sudo zypper install -y hyprland hyprpaper hyprcursor hypridle hyprlock hyprshot hyprpicker
