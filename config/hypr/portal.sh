c686f164c365e97d88d1ea3ac7ab8fab901d61ad
christian80gabi <christian-gabriel.nana@outlook.com>
2024-08-03 15:11:19 +0100

    portal+clipboard: fixing portals and adding clipbloard manager

--- config/hypr/portal.sh
+++ config/hypr/portal.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
 sleep 1
-echo "Kill portals..."
-killall -e xdg-desktop-portal-hyprland
-killall -e xdg-desktop-portal-wlr
-killall xdg-desktop-portal
-
-sleep 1
 echo "Start portals..."
 /usr/libexec/xdg-desktop-portal-hyprland &
 sleep 2
