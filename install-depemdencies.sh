48ed1d153c3730612a10a04972f6a1b2ba189793
Christian Gabi <49684157+christian80gabi@users.noreply.github.com>
2024-08-03 13:41:01 +0100

    Fixes and updates (#1)
    - Adding fixing portals
    - Adding swaylock config
    - Adding blueman as bluetooth manager
    - Fixing keyboard shortcuts scripts for volume up and down and mute

--- install-depemdencies.sh
+++ install-depemdencies.sh
@@ -1,7 +1,7 @@
 !/usr/bin/env bash
 
 echo "Installing openSUSEway and dependecies. (This will configure a lot for you)"
-sudo zypper install -y --recommends openSUSEway patterns-openSUSEway qt6ct
+sudo zypper install -y --recommends openSUSEway patterns-openSUSEway qt6ct btop blueman
 
 echo "Installing Hyprland"
 sudo zypper install -y hyprland hyprpaper
