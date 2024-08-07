f270a2f2efa64c2e3c0cd962628f6eb4cc462c0a
christian80gabi <christian-gabriel.nana@outlook.com>
2024-08-07 21:13:44 +0100

    theme: fix theme setting qt platform to kde - adding more dependencies

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,7 +1,7 @@
 !/usr/bin/env bash
 
 echo "Installing openSUSEway and dependencies. (This will configure a lot for you)"
-sudo zypper install -y --recommends openSUSEway patterns-openSUSEway qt6ct btop blueman clipman 
+sudo zypper install -y --recommends openSUSEway patterns-openSUSEway
 
 echo "Installing Hyprland"
-sudo zypper install -y hyprland hyprpaper hyprcursor hypridle hyprlock hyprshot hyprpicker
+sudo zypper install -y hyprland hyprpaper hyprcursor hypridle hyprlock hyprshot hyprpicker kvantum-manager kvantum-qt5 kvantum-themes qt6ct btop blueman clipman nwg-look
