97777b3bea12f5448657defd59e94f5fcd6dccf7
christian80gabi <christian-gabriel.nana@outlook.com>
2025-06-07 19:10:56 +0100

    dependencies: update install-dependencies.sh to enhance installation process and add missing utilities

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,7 +1,26 @@
 !/usr/bin/env bash
+# This script installs dependencies for Hyprland on openSUSE Tumbleweed.
 
-echo "Installing openSUSEway and dependencies. (This will configure a lot for you)"
-sudo zypper install -y --recommends openSUSEway patterns-openSUSEway
+echo "Refreshing zypper database"
+sudo zypper clean && sudo zypper refresh
+
+echo "Update system"
+sudo zypper dup -y
 
 echo "Installing Hyprland"
-sudo zypper install -y hyprland hyprpaper hyprcursor hypridle hyprlock hyprshot hyprpicker kvantum-manager kvantum-qt5 kvantum-themes qt6ct btop blueman clipman nwg-look kitty
+sudo zypper install -y hyprland 
+
+echo "Installing Hyprland utilities"
+sudo zypper install hyprpolkitagent hyprpaper hyprcursor hypridle hyprlock hyprshot hyprpicker kitty xdg-menu
+
+echo "Installing QT & GTK Support tools"
+sudo zypper install -y hyprland-qtutils kvantum-manager kvantum-themes hyprland-qt-support qt6-wayland
+
+echo "Tools you may want to install (especially if you don't use KDE)"
+sudo zypper install -y xdg-desktop-portal-gtk xdg-desktop-portal-kde xdg-desktop-portal-wlr xdg-desktop-portal-hyprland qt6ct qt5ct nwg-look
+
+echo "Installing other utilities"
+sudo zypper install -y dunst waybar wofi bluetui cliphist wl-clipboard brightnessctl playerctl NetworkManager-applet
+
+echo "Installing Fonts"
+sudo zypper install -y google-noto-fonts google-noto-coloremoji-fonts google-noto-sans-cjk-fonts jetbrains-mono-fonts fira-code-fonts google-inconsolata-fonts google-caladea-fonts saja-cascadia-code-fonts 
